71eb5141f534813dc813e8604e893c7a96b8b47c
Immaculate Delali <florenceahorlu@meltwater.org>
2022-10-12 11:18:13 +0000

    class trials

---
+++ Adding.java
@@ -0,0 +1,18 @@
+public class Adding {
+   
+    public static void main(String[] args){
+        testVariables();
+    System.out.println("Higher ranking");
+}
+
+public static void testVariables(){
+    int sampleint = 20;
+    float floatNumber = 12.5F;
+    boolean hasPass = true;
+    String name = "Ahorlu Delali";
+
+    double doublecasted = sampleint;
+    System.out.println("here" + doublecasted);
+}
+
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main{
+public static void main(String[] args){
+    int examScore = 90;
+   /*  float floatNumber = 12.5F;
+    boolean hasPass = true;
+    String name = "Ahorlu Delali";
+    */
+    double dexamsScore = examScore;
+    int intexamsScore = (int) dexamsScore;
+    Test.displayResult();
+    System.out.println("Hello world!" + String.valueOf(intexamsScore));
+   //another();
+    }
+   /* public static void another(){
+      System.out.println("Testing another main");
+    }*/
+}
+
+
